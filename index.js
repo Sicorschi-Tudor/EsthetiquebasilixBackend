fa8de06aab9c1d580e11638bd310b1a4f17a187c
Sicorschi-Tudor <aaw1713tudor@gmail.com>
2024-05-29 17:11:24 +0300

    asd

--- index.js
+++ index.js
@@ -24,55 +24,47 @@ fs.readFile("./reservation.json", "utf8", (err, data) => {
   }
 })
 
-app.get("https://esthetiquebasilixbackend.onrender.com/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Server is ready")
 })
 
-app.get(
-  "https://esthetiquebasilixbackend.onrender.com/api/reservation",
-  (req, res) => {
-    res.json(reservation)
-  }
-)
+app.get("/api/reservation", (req, res) => {
+  res.json(reservation)
+})
 
-app.post(
-  "https://esthetiquebasilixbackend.onrender.com/api/reservation",
-  (req, res) => {
-    const newReservation = { id: uuidv4(), ...req.body }
-    reservation.push(newReservation)
+app.post("/api/reservation", (req, res) => {
+  const newReservation = { id: uuidv4(), ...req.body }
+  reservation.push(newReservation)
 
-    fs.writeFile("./reservation.json", JSON.stringify(reservation), err => {
-      if (err) {
-        console.error("Error saving reservation data:", err)
-        res.status(500).send("Error saving reservation data")
-      } else {
-        res.status(201).json(newReservation)
-      }
+  fs.writeFile("./reservation.json", JSON.stringify(reservation), err => {
+    if (err) {
+      console.error("Error saving reservation data:", err)
+      res.status(500).send("Error saving reservation data")
+    } else {
+      res.status(201).json(newReservation)
+    }
+  })
+})
+
+app.post("/api/send-email", async (req, res) => {
+  const { name, surname, tel, service, email, data, time } = req.body
+
+  try {
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "companybrizy@gmail.com",
+        pass: "Xiaomitudor1.",
+      },
     })
-  }
-)
 
-app.post(
-  "https://esthetiquebasilixbackend.onrender.com//api/send-email",
-  async (req, res) => {
-    const { name, surname, tel, service, email, data, time } = req.body
-
-    try {
-      // Create a transporter
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: "companybrizy@gmail.com",
-          pass: "Xiaomitudor1.",
-        },
-      })
-
-      // Send mail with defined transport object
-      await transporter.sendMail({
-        from: "companybrizy@gmail.com", // sender address
-        to: "aaw1713tudor@gmail.com", // list of receivers
-        subject: "New Contact Form Submission", // Subject line
-        html: `
+    // Send mail with defined transport object
+    await transporter.sendMail({
+      from: "companybrizy@gmail.com", // sender address
+      to: "aaw1713tudor@gmail.com", // list of receivers
+      subject: "New Contact Form Submission", // Subject line
+      html: `
         <p>Name: ${name}</p>
         <p>Surname: ${surname}</p>
         <p>Telephone: ${tel}</p>
@@ -81,15 +73,14 @@ app.post(
         <p>Date: ${data}</p>
         <p>Time: ${time}</p>
       `, // HTML body
-      })
+    })
 
-      res.status(200).send("Email sent successfully")
-    } catch (error) {
-      console.error("Error sending email:", error)
-      res.status(500).send("Failed to send email")
-    }
+    res.status(200).send("Email sent successfully")
+  } catch (error) {
+    console.error("Error sending email:", error)
+    res.status(500).send("Failed to send email")
   }
-)
+})
 
 const port = 5000
 app.listen(port, () => {
