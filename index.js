4d9b59ac14373a9ac32ae176dde9951e138d59ca
Sicorschi-Tudor <aaw1713tudor@gmail.com>
2024-05-29 17:07:42 +0300

    asd

--- index.js
+++ index.js
@@ -1,70 +1,78 @@
-import express from "express";
-import cors from "cors";
-import nodemailer from "nodemailer"; // Import nodemailer without destructuring
-import bodyParser from "body-parser";
-import { v4 as uuidv4 } from "uuid";
-import fs from "fs";
+import express from "express"
+import cors from "cors"
+import nodemailer from "nodemailer" // Import nodemailer without destructuring
+import bodyParser from "body-parser"
+import { v4 as uuidv4 } from "uuid"
+import fs from "fs"
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
 
-let reservation = [];
+let reservation = []
 
 fs.readFile("./reservation.json", "utf8", (err, data) => {
   if (err) {
-    console.error("Error reading reservation data:", err);
+    console.error("Error reading reservation data:", err)
   } else {
     try {
-      reservation = JSON.parse(data);
+      reservation = JSON.parse(data)
     } catch (parseErr) {
-      console.error("Error parsing reservation data:", parseErr);
+      console.error("Error parsing reservation data:", parseErr)
     }
   }
-});
+})
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.get("https://esthetiquebasilixbackend.onrender.com/", (req, res) => {
+  res.send("Server is ready")
+})
 
-app.get("/api/reservation", (req, res) => {
-  res.json(reservation);
-});
+app.get(
+  "https://esthetiquebasilixbackend.onrender.com/api/reservation",
+  (req, res) => {
+    res.json(reservation)
+  }
+)
 
-app.post("/api/reservation", (req, res) => {
-  const newReservation = { id: uuidv4(), ...req.body };
-  reservation.push(newReservation);
+app.post(
+  "https://esthetiquebasilixbackend.onrender.com/api/reservation",
+  (req, res) => {
+    const newReservation = { id: uuidv4(), ...req.body }
+    reservation.push(newReservation)
 
-  fs.writeFile("./reservation.json", JSON.stringify(reservation), (err) => {
-    if (err) {
-      console.error("Error saving reservation data:", err);
-      res.status(500).send("Error saving reservation data");
-    } else {
-      res.status(201).json(newReservation);
-    }
-  });
-});
+    fs.writeFile("./reservation.json", JSON.stringify(reservation), err => {
+      if (err) {
+        console.error("Error saving reservation data:", err)
+        res.status(500).send("Error saving reservation data")
+      } else {
+        res.status(201).json(newReservation)
+      }
+    })
+  }
+)
 
-app.post("/api/send-email", async (req, res) => {
-  const { name, surname, tel, service, email, data, time } = req.body;
+app.post(
+  "https://esthetiquebasilixbackend.onrender.com//api/send-email",
+  async (req, res) => {
+    const { name, surname, tel, service, email, data, time } = req.body
 
-  try {
-    // Create a transporter
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "companybrizy@gmail.com",
-        pass: "Xiaomitudor1.",
-      },
-    });
+    try {
+      // Create a transporter
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "companybrizy@gmail.com",
+          pass: "Xiaomitudor1.",
+        },
+      })
 
-    // Send mail with defined transport object
-    await transporter.sendMail({
-      from: "companybrizy@gmail.com", // sender address
-      to: "aaw1713tudor@gmail.com", // list of receivers
-      subject: "New Contact Form Submission", // Subject line
-      html: `
+      // Send mail with defined transport object
+      await transporter.sendMail({
+        from: "companybrizy@gmail.com", // sender address
+        to: "aaw1713tudor@gmail.com", // list of receivers
+        subject: "New Contact Form Submission", // Subject line
+        html: `
         <p>Name: ${name}</p>
         <p>Surname: ${surname}</p>
         <p>Telephone: ${tel}</p>
@@ -73,16 +81,17 @@ app.post("/api/send-email", async (req, res) => {
         <p>Date: ${data}</p>
         <p>Time: ${time}</p>
       `, // HTML body
-    });
+      })
 
-    res.status(200).send("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).send("Failed to send email");
+      res.status(200).send("Email sent successfully")
+    } catch (error) {
+      console.error("Error sending email:", error)
+      res.status(500).send("Failed to send email")
+    }
   }
-});
+)
 
-const port = 5000;
+const port = 5000
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
